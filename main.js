c6856c25e0c38865e732528bbe26fcc0650e92ad
IvabGaming <ivanosincorporated@gmail.com>
2026-04-19 07:14:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const fs = require('fs');
-const path = require('path');
+const Redis = require('ioredis');
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-
-    win.loadFile('index.html');
-}
-
-ipcMain.on('load-history', (event) => {
-    const filePath = path.join(__dirname, 'messages.txt');
-
-    if (fs.existsSync(filePath)) {
-        fs.readFile(filePath, 'utf-8', (err, data) => {
-            if (err) throw err;
-            event.replay('history-data', data);
-
-ipcMain.on('save-message', (event, message) => {
-    const filePath = path.join(__dirname, 'messages.txt');
-    fs.appendFile(filePath, message + '\n', (err) => {
-        if (err) throw err;
-        console.log('Saved to file!');
-    });
+const redis = new Redis({
+  host: 'redis-18531.c262.us-east-1-3.ec2.cloud.redislabs.com',
+  port: 18531,
+  password: 'JqSKZX2mtm68KsfU62QHEc5IY1IZiDrC'
 });
 
-app.whenReady().then(createWindow);
+const sub = new Redis({
+  host: 'redis-18531.c262.us-east-1-3.ec2.cloud.redislabs.com',
+  port: 18531,
+  password: 'JqSKZX2mtm68KsfU62QHEc5IY1IZiDrC'
+});
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  mainWindow.loadFile('index.html');
+
+  sub.subscribe('chat');
+  sub.on('message', (channel, msg) => {
+    mainWindow.webContents.send('message', msg);
+  });
+});
+
+ipcMain.on('send-message', (event, msg) => {
+  redis.publish('chat', msg);
+  redis.lpush('history', msg);
+  redis.ltrim('history', 0, 99); // Keep only the latest 100 messages
+});
+
+ipcMain.on('get-history', (event) => {
+	  redis.lrange('history', 0, -1, (err, messages) => {
+		messages.reverse(); // Reverse to show oldest first
+		event.reply('history', messages);
+	  });
+})
